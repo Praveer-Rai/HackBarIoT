9fd7288d70696c1d5a7713b4764d7bb1d9cf1c9c
bsautermeister <apps@bsautermeister.de>
2017-11-17 23:07:44 +0100

    Added basic NFC id reading functionality

--- frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/MainActivity.java
+++ frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/MainActivity.java
@@ -1,13 +1,122 @@
 package hackbar.de.hackbardroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+import hackbar.de.hackbardroid.utils.NfcUtils;
+
+public class MainActivity extends Activity {
+
+    public static final String TAG = "NfcDemo";
+
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC is disabled.", Toast.LENGTH_LONG).show();
+        }
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        NfcUtils.setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        NfcUtils.stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            String type = intent.getType();
+            if (type.equals(NfcUtils.MIME_TEXT_PLAIN)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new RFIDReaderTask().execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = NfcA.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new RFIDReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+    }
+
+    private class RFIDReaderTask extends AsyncTask<Tag, Void, Long> {
+
+        @Override
+        protected Long doInBackground(Tag... params) {
+            Tag tag = params[0];
+            byte[] idData = tag.getId();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte i : idData) {
+                sb.append(String.format(Locale.ENGLISH, "%d", i + 128));
+            }
+            return Long.valueOf(sb.toString());
+        }
+
+        @Override
+        protected void onPostExecute(Long result) {
+            if (result != null) {
+                Toast.makeText(MainActivity.this, "ID: " + result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
---
+++ frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/utils/NfcUtils.java
@@ -0,0 +1,53 @@
+package hackbar.de.hackbardroid.utils;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+
+public class NfcUtils {
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+
+    private NfcUtils() {}
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[3][1];
+        techList[0][0] = "android.nfc.tech.NdefFormatable";
+        techList[1][0] = "android.nfc.tech.NfcA";
+        techList[2][0] = "android.nfc.tech.MifareClassic";
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
+        filters[0].addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList); //filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding BaseActivity requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+}
