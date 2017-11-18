0189fbd5cbac061920d06ecd1497d16b22afb6e2
bsautermeister <apps@bsautermeister.de>
2017-11-18 12:13:14 +0100

    Added imple login mechanism

--- frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/MainActivity.java
+++ frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/MainActivity.java
@@ -1,13 +1,19 @@
 package hackbar.de.hackbardroid;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import hackbar.de.hackbardroid.settings.UserSettings;
 import hackbar.de.hackbardroid.utils.NfcUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,24 @@ public class MainActivity extends Activity {
             finish();
         }
 
+        if (!checkLoggedIn())
+            return;
+
         handleIntent(getIntent());
+    }
+
+    private boolean checkLoggedIn() {
+        UserSettings userSettings = new UserSettings(getApplicationContext());
+        if (userSettings.getUserId() == null) {
+            logout();
+            return false;
+        }
+        return true;
+    }
+
+    private void logout() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -46,6 +69,28 @@ public class MainActivity extends Activity {
 
         if (id != null) {
             Toast.makeText(MainActivity.this, "ID: " + id, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
     }
 }
