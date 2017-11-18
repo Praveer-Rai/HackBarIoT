aab02e8e34263d54c206fb442e8ff0921c4f8c5c
bsautermeister <apps@bsautermeister.de>
2017-11-18 15:53:17 +0100

    Added background video and logo in login page.

--- frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/LoginActivity.java
+++ frontend/HackBarDroid/app/src/main/java/hackbar/de/hackbardroid/LoginActivity.java
@@ -2,16 +2,22 @@ package hackbar.de.hackbardroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import hackbar.de.hackbardroid.settings.UserSettings;
 
 public class LoginActivity extends Activity {
 
     private EditText usernameText;
+    private VideoView backgroundVideo;
 
     private UserSettings userSettings;
 
@@ -21,6 +27,28 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         usernameText = findViewById(R.id.usernameText);
+        usernameText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                loginClicked(textView);
+                return true;
+            }
+        });
+        backgroundVideo = findViewById(R.id.backgroundVideo);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.barteaser);
+
+        backgroundVideo.setDrawingCacheEnabled(true);
+        backgroundVideo.setVideoURI(uri);
+        backgroundVideo.requestFocus();
+        backgroundVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                mp.setVolume(0, 0);
+            }
+        });
+
         userSettings = new UserSettings(getApplicationContext());
     }
 
@@ -28,8 +56,17 @@ public class LoginActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
+        backgroundVideo.start();
+
         // when we arrive here, delete the user (from prev login)
         userSettings.setUserId(null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        backgroundVideo.pause();
     }
 
     public void loginClicked(View view) {
